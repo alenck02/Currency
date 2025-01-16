a9f744fe59923768a174d65b2e93b277ef1623ba
alenck <wjdgkdusjeong@naver.com>
2025-01-16 22:40:32 +0900

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation("com.google.code.gson:gson:2.8.5")
+
+    implementation("androidx.navigation:navigation-compose:2.8.5")
 }
--- app/src/main/java/com/example/currency/MainActivity.kt
+++ app/src/main/java/com/example/currency/MainActivity.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.currency.ui.theme.CurrencyTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,49 +30,70 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CurrencyTheme {
-                uiPreview()
+                AppNavigation()
             }
         }
     }
 }
 
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "main") {
+        composable("main") { uiPreview(navController) }
+        composable("countrySelection") { CountrySelectionScreen(navController) }
+    }
+}
+
+@Composable
+fun uiPreview(navController: NavHostController) {
+    Column {
+        CountryItem("KRW", navController)
+        CountryItem("USD", navController)
+    }
+}
+
+@Composable
+fun CountryItem(currency: String, navController: NavHostController) {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(10.dp)
+            .padding(top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding())
+            .clickable { navController.navigate("countrySelection") },
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = currency)
+        Text(text = "0")
+    }
+}
+
+@Composable
+fun CountrySelectionScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .padding(top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding())
+    ) {
+        Text(text = "나라 선택 화면입니다.")
+
+        Text(
+            text = "나라 선택 완료",
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .clickable {
+                    navController.popBackStack()
+                }
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun uiPreview() {
-    Column {
-        country()
-        country1()
-    }
-}
-
-@Composable
-fun country() {
-    Row(
-        modifier = Modifier.fillMaxWidth()
-            .padding(10.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(
-            text = "KRW"
-        )
-        Text(
-            text = "0"
-        )
-    }
-}
-
-@Composable
-fun country1() {
-    Row(
-        modifier = Modifier.fillMaxWidth()
-            .padding(10.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(
-            text = "USD"
-        )
-        Text(
-            text = "0"
-        )
+fun PreviewCountryItems() {
+    CurrencyTheme {
+        Column {
+            CountryItem("KRW", rememberNavController())
+            CountryItem("USD", rememberNavController())
+        }
     }
 }
