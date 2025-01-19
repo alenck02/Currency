1c6bc7ac7f4a82772f96db62bb88389455eee563
alenck <wjdgkdusjeong@naver.com>
2025-01-19 22:10:23 +0900

    commit

--- app/src/main/java/com/example/currency/MainActivity.kt
+++ app/src/main/java/com/example/currency/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.currency
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
